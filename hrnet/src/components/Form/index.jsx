e8964f0e5a588bca61374bf94f3ab52b5c567f85
POLETTI Stéphane <polettistephane@msn.com>
2023-01-03 10:40:31 +0100

    media query page home styled components form styled components date picker

--- hrnet/src/components/Form/index.jsx
+++ hrnet/src/components/Form/index.jsx
@@ -25,59 +25,58 @@ const Form = () => {
   };
   return (
     <StyledForm id="form" onSubmit={onSubmit}>
-      <div className="form-employee-details">
+      <StyledDivFormEmployeeDetails className="form-employee-details">
         <div className="form-employee-details-personnal">
-          <label htmlFor="first-name">First Name</label>
-          <input
+          <StyledLabel htmlFor="first-name">First Name</StyledLabel>
+          <StyledFormInput
             type="text"
             id="first-name"
             onChange={(e) => setFirstNameEmployee(e.target.value)}
             required
           />
 
-          <label htmlFor="last-name">Last Name</label>
-          <input
+          <StyledLabel htmlFor="last-name">Last Name</StyledLabel>
+          <StyledFormInput
             type="text"
             id="last-name"
             onChange={(e) => setLastNameEmployee(e.target.value)}
             required
           />
-          <label htmlFor="date-of-birth">Date of Birth</label>
-          {/* <input id="date-of-birth" type="text" required /> */}
-          {/* <fieldset className="address">
-              <legend>Address</legend> */}
-          <DatePicker
-            id="date-of-birth"
-            selected={birthDateEmployee}
-            onChange={(date) => setBirthDateEmployee(date)}
-            // minDate={subDays(birthDayEmployee, 365)}
-            // maxDate={moment().subtract(18, "years").format("MM-DD-YYYY")}
-            // filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
-            // showDisabledMonthNavigation
-            // showYearDropdown={true}
-            // scrollableMonthYearDropdown
-            isClearable
-            required
-          />
+          <StyledLabel htmlFor="date-of-birth">Date of Birth</StyledLabel>
+          <StyledWrapperDatepicker>
+            <DatePicker
+              id="date-of-birth"
+              selected={birthDateEmployee}
+              onChange={(date) => setBirthDateEmployee(date)}
+              // minDate={subDays(birthDayEmployee, 365)}
+              // maxDate={moment().subtract(18, "years").format("MM-DD-YYYY")}
+              // filterDate={(date) => date.getDay() !== 6 && date.getDay() !== 0}
+              // showDisabledMonthNavigation
+              showYearDropdown={true}
+              scrollableMonthYearDropdown={true}
+              isClearable
+              required
+            />
+          </StyledWrapperDatepicker>
 
-          <label htmlFor="street">Street</label>
-          <input
+          <StyledLabel htmlFor="street">Street</StyledLabel>
+          <StyledFormInput
             id="street"
             type="text"
             onChange={(e) => setStreetEmployee(e.target.value)}
             required
           />
 
-          <label htmlFor="city">City</label>
-          <input
+          <StyledLabel htmlFor="city">City</StyledLabel>
+          <StyledFormInput
             id="city"
             type="text"
             onChange={(e) => setCityEmployee(e.target.value)}
             required
           />
 
-          <label htmlFor="state">State</label>
-          <select
+          <StyledLabel htmlFor="state">State</StyledLabel>
+          <StyledFormSelect
             name="state"
             id="state"
             onChange={(e) => setStateEmployee(e.target.value)}
@@ -87,11 +86,10 @@ const Form = () => {
                 {item.name}
               </option>
             ))}
-            {/* <option>Select a State</option> */}
-          </select>
+          </StyledFormSelect>
 
-          <label htmlFor="zip-code">Zip Code</label>
-          <input
+          <StyledLabel htmlFor="zip-code">Zip Code</StyledLabel>
+          <StyledFormInput
             id="zip-code"
             type="number"
             onChange={(e) => setZipCodeEmployee(e.target.value)}
@@ -100,18 +98,21 @@ const Form = () => {
           {/* </fieldset> */}
         </div>
         <div className="form-employee-details-job">
-          <label htmlFor="start-date">Start Date</label>
-          {/* <input id="start-date" type="text" required /> */}
-          <DatePicker
-            id="start-date"
-            selected={StartDateEmployee}
-            onChange={(date) => setStartDateEmployee(date)}
-            isClearable
-            required
-          />
+          <StyledLabel htmlFor="start-date">Start Date</StyledLabel>
+          <StyledWrapperDatepicker>
+            <DatePicker
+              id="start-date"
+              selected={StartDateEmployee}
+              onChange={(date) => setStartDateEmployee(date)}
+              showYearDropdown={true}
+              scrollableMonthYearDropdown={true}
+              isClearable
+              required
+            />
+          </StyledWrapperDatepicker>
 
-          <label htmlFor="department">Department</label>
-          <select
+          <StyledLabel htmlFor="department">Department</StyledLabel>
+          <StyledFormSelect
             name="department"
             id="department"
             onChange={(e) => setDepartmentEmployee(e.target.value)}
@@ -121,12 +122,12 @@ const Form = () => {
                 {item.name}
               </option>
             ))}
-          </select>
-          <button type="submit" value="submit" id="btn-submit">
+          </StyledFormSelect>
+          <StyledButtonSubmit type="submit" value="submit" id="btn-submit">
             Save
-          </button>
+          </StyledButtonSubmit>
         </div>
-      </div>
+      </StyledDivFormEmployeeDetails>
     </StyledForm>
   );
 };
@@ -139,4 +140,111 @@ const StyledForm = styled.form`
   justify-content: center;
   text-align: center;
   width: 100%;
+`;
+
+const StyledLabel = styled.label`
+  display: flex;
+  margin-left: 10px;
+  width: fit-content;
+`;
+
+const StyledDivFormEmployeeDetails = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+  @media (max-width: 1050px) {
+    flex-direction: column;
+  }
+`;
+
+const StyledFormInput = styled.input`
+  display: flex;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  margin: 0;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  padding-left: 15px;
+  background-color: rgb(226, 232, 227);
+  font-size: 25px;
+  border: none;
+  border-radius: 20px;
+  border-bottom: 2px solid black;
+  border-right: 2px solid black;
+  @media (max-width: 1050px) {
+    font-size: 12px;
+    width: 80%;
+  }
+`;
+
+const StyledFormSelect = styled.select`
+  display: flex;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 300px;
+  margin: 0;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  padding-left: 15px;
+  background-color: rgb(226, 232, 227);
+  font-size: 25px;
+  border: none;
+  border-radius: 20px;
+  border-bottom: 2px solid black;
+  border-right: 2px solid black;
+  @media (max-width: 1050px) {
+    font-size: 12px;
+    width: 80%;
+  }
+`;
+
+const StyledWrapperDatepicker = styled.div`
+  .react-datepicker {
+    box-shadow: 3px 2px 11px rgb(0 0 0 / 30%);
+  }
+  .react-datepicker__input-container input {
+    display: flex;
+    height: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 300px;
+    margin: 0;
+    margin-top: 5px;
+    margin-bottom: 10px;
+    padding-left: 15px;
+    background-color: rgb(226, 232, 227);
+    font-size: 25px;
+    border: none;
+    border-radius: 20px;
+    border-bottom: 2px solid black;
+    border-right: 2px solid black;
+    @media (max-width: 1050px) {
+      font-size: 12px;
+      width: 80%;
+    }
+  }
+`;
+
+const StyledButtonSubmit = styled.button`
+  border-radius: 20px;
+  margin: auto;
+  margin-top: 80px;
+  width: 200px;
+  height: 50px;
+  background-color: #6d8211;
+  color: white;
+  font-size: 30px;
+  cursor: pointer;
+  &:hover {
+    color: #6d8211;
+    background-color: white;
+    border: 5px solid #6d8211;
+    font-weight: 700;
+  }
 `;
