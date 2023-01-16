42db47d11986e7aa9ef731af65abb6d9d23a42d8
POLETTI Stéphane <polettistephane@msn.com>
2023-01-16 12:48:28 +0100

    fix yarn

--- hrnet/src/components/Nav/index.jsx
+++ hrnet/src/components/Nav/index.jsx
@@ -1,10 +1,8 @@
 import "./style.scss";
-import logo from "../../assets/logo.png";
 
 const Nav = () => {
   return (
     <nav className="nav">
-      <img src={logo} alt="logo Wealth Health" className="logo" />
       <h1 className="logo-name">WEALTH HEALTH</h1>
     </nav>
   );
